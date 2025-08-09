9a68d77bae7af3c27085b9c99f36e4dc13d50eac
Ramish Munir <munirramish3@gmail.com>
2025-08-08 22:01:30 -0400

    working dark mode

--- components/ui/themetoggle.jsx
+++ components/ui/themetoggle.jsx
@@ -1,10 +1,22 @@
-"use client"
+"use client";
 import { useTheme } from "next-themes";
+import { Sun, Moon } from "lucide-react";
+import { Switch } from "./switch";
+import { useEffect, useState } from "react";
 
 export default function ThemeToggle(){
     const {theme, setTheme} = useTheme();
-    
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
     return(
-        <div>theme</div>
+        <div className="flex items-center space-x-2">
+            <Sun className={`${theme === "light" ? "text-yellow-600" : ""}`}></Sun>
+            {mounted && <Switch checked={theme === "dark"} onCheckedChange={(checked) => setTheme(checked ? "dark" : "light")}></Switch> }            
+            <Moon></Moon>
+        </div>
     );
 }
